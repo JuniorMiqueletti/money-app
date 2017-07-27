d53ff014fba6a30a5b3a599e382222a7c4c827e0
Junior Miqueletti <juniormiqueletti@gmail.com>
2017-07-26 21:12:46 -0300

    Improving CategoryController

--- money-api/src/main/java/com/juniormiqueletti/moneyapp/controller/CategoryController.java
+++ money-api/src/main/java/com/juniormiqueletti/moneyapp/controller/CategoryController.java
@@ -1,11 +1,21 @@
 package com.juniormiqueletti.moneyapp.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.juniormiqueletti.moneyapp.model.Category;
 import com.juniormiqueletti.moneyapp.repository.CategoryRepository;
@@ -16,9 +26,31 @@ public class CategoryController {
 
 	@Autowired
 	private CategoryRepository repo;
-	
+
 	@GetMapping
-	public List<Category> listAll(){
-		return repo.findAll();
+	public List<Category> listAll() {
+
+		List<Category> categories = repo.findAll();
+		return categories;
+	}
+
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+
+		Category created = repo.save(category);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(created.getId())
+				.toUri();
+
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(created);
+	}
+
+	@GetMapping("/{id}")
+	public Category findById(@PathVariable Long id) {
+		
+		return repo.findOne(id);
 	}
 }
