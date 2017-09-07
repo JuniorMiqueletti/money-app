845b03668e12a06a3a0439ac5f33e469487b4529
Junior Miqueletti <juniormiqueletti@gmail.com>
2017-09-07 09:41:35 -0300

    Configuring to use basic oauth

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# money-app
-Money App
+# Money App
+## Specs
+* /money-api - Server Side Application
+
+### Technologies
+* Java 8
+* Spring Framework _(Boot, Data, Security, Oauth, DevTools, Web)_
+* Mysql
+* Flyway Db Migrator
+* Jackson
+* Apache Commons
+
+#### How to use
+
+##### Postman tests
+ 1. Get client **oauth-tokken** with _basic authentication_.
+``` POST: localhost:8080/oauth/token
+Authorization Type : Basic Oauth
+user: angular
+password: @ngul@r
+
+BODY > x-www-form-urlencoded
+
+client: angular
+username: admin
+password: admin
+grant_type > password
+```
+2. Send in new requests with **HEADER Key** _"Authorization"_ with bearer token ("bearer xxxxxnnnnxxxnnnxxx-xnxnnx-")
--- money-api/src/main/java/com/juniormiqueletti/moneyapp/config/ResourceServerConfig.java
+++ money-api/src/main/java/com/juniormiqueletti/moneyapp/config/ResourceServerConfig.java
@@ -1,16 +1,19 @@
 package com.juniormiqueletti.moneyapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
-	@Override
+	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 			.withUser("admin")
@@ -19,12 +22,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/category").permitAll()
 			.anyRequest().authenticated()
-			.and().httpBasic()
 			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and().csrf().disable();
+	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
 	}
 }
