0ed031d4f246e11f0c00eb48e9731b0dcf4b8ca9
Junior Miqueletti <junior.miqueletti@matera.com>
2018-03-02 19:33:58 -0300

    adjusting artifactId and removing unnecessary imports

--- src/main/java/com/juniormiqueletti/moneyapp/cors/CorsFilter.java
+++ src/main/java/com/juniormiqueletti/moneyapp/cors/CorsFilter.java
@@ -4,7 +4,6 @@ import com.juniormiqueletti.moneyapp.config.property.MoneyApiProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
