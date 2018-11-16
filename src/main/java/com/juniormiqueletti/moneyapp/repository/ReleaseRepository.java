07217582b4304df7033764798432930c72013389
Junior Miqueletti <juniormiqueletti@gmail.com>
2018-11-16 20:02:43 -0200

    Improving ReleaseRepository and UserRepository

--- src/main/java/com/juniormiqueletti/moneyapp/repository/ReleaseRepository.java
+++ src/main/java/com/juniormiqueletti/moneyapp/repository/ReleaseRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.juniormiqueletti.moneyapp.model.Release;
 import com.juniormiqueletti.moneyapp.repository.release.ReleaseRepositoryQuery;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ReleaseRepository extends JpaRepository<Release, Long>, ReleaseRepositoryQuery {
 
+    public List<Release> findByDueDateLessThanEqualAndPayDateIsNull(final LocalDate date);
 }
--- src/main/java/com/juniormiqueletti/moneyapp/repository/UserRepository.java
+++ src/main/java/com/juniormiqueletti/moneyapp/repository/UserRepository.java
@@ -3,9 +3,12 @@ package com.juniormiqueletti.moneyapp.repository;
 import com.juniormiqueletti.moneyapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+    public Optional<User> findByEmail(final String email);
+
+    public List<User> findByPermissionListDescription(final String descriptionPermission);
 }
