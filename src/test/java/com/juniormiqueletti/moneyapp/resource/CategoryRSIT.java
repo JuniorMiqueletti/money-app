b1cb0cfe9893aac1d6b68830068bb1093b321e98
Junior Miqueletti <juniormiqueletti@gmail.com>
2018-11-25 16:48:43 -0200

    Fixing CategoryRSIT

--- src/test/java/com/juniormiqueletti/moneyapp/resource/CategoryRSIT.java
+++ src/test/java/com/juniormiqueletti/moneyapp/resource/CategoryRSIT.java
@@ -1,38 +1,31 @@
 package com.juniormiqueletti.moneyapp.resource;
 
-import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.junit.Test;
 
 import java.util.Base64;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
-public class CategoryRsIT {
+public class CategoryRSIT {
 
     private static final String URL_OAUTH = "/oauth/token";
     private static final String CONTENT_TYPE_HEADER = "Content-Type";
     private static final String APPLICATION_JSON = "application/json";
-    private static final int OK_HTTP_STATUS_CODE = 200;
     private static final int UNAUTHORIZED_HTTP_STATUS_CODE = 401;
-    private static final int BAD_REQUEST_HTTP_STATUS_CODE = 400;
-    private static final String CATEGORY_URL = "/category";
-
-    private int portNumber = 8080;
+    private static final String CATEGORY_URL = "/api/v1/category";
 
     @Test
     public void requestCategoryWithoutToken() {
 
         Response response =
-            RestAssured
-                .given()
-                .port(portNumber)
+            given()
                 .header("Accept", APPLICATION_JSON)
                 .header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
                 .get(CATEGORY_URL)
-                .thenReturn();
+            .thenReturn();
 
         String errorDescription = response.getBody().jsonPath().getString("error_description");
 
@@ -46,16 +39,15 @@ public class CategoryRsIT {
             Base64.getEncoder().encodeToString("angular:@ngul@r".getBytes());
 
         Response response =
-            RestAssured.given()
-                .port(portNumber)
+            given()
                 .header(new Header("Authorization", "Basic " + clientBasicAuthCredentials))
                 .queryParam("username", username)
                 .queryParam("password", password)
                 .queryParam("grant_type", "password")
-                .when()
+            .when()
                 .post(URL_OAUTH)
-                .then()
-                    .extract().response();
+            .then()
+                .extract().response();
 
         return response;
     }
