78b8ec5e05b91cf51b902b083c29c8f240336bc5
Junior Miqueletti <juniormiqueletti@gmail.com>
2018-12-03 22:24:28 -0200

    Imp get Category not found test

--- src/test/java/com/juniormiqueletti/moneyapp/resource/CategoryRSIT.java
+++ src/test/java/com/juniormiqueletti/moneyapp/resource/CategoryRSIT.java
@@ -3,9 +3,11 @@ package com.juniormiqueletti.moneyapp.resource;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 public class CategoryRSIT extends AuthenticableTest {
 
@@ -22,8 +24,8 @@ public class CategoryRSIT extends AuthenticableTest {
             given()
                 .header("Accept", APPLICATION_JSON)
                 .header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
-                .get(CATEGORY_URL)
-            .thenReturn();
+            .get(CATEGORY_URL)
+                .thenReturn();
 
         String errorDescription = response.getBody().jsonPath().getString("error_description");
 
@@ -32,7 +34,7 @@ public class CategoryRSIT extends AuthenticableTest {
     }
 
     @Test
-    public void requestACategory() {
+    public void requestAExistedCategory_shouldReturnOk() {
 
         Header authenticationHeader = getAuthenticationHeader();
 
@@ -41,9 +43,25 @@ public class CategoryRSIT extends AuthenticableTest {
                 .header("Accept", APPLICATION_JSON)
                 .header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
                 .header(authenticationHeader)
-                .get(CATEGORY_URL + "/1")
+            .get(CATEGORY_URL + "/1")
                 .thenReturn();
 
         assertEquals(OK_HTTP_STATUS_CODE, response.getStatusCode());
+    }
+
+    @Test
+    public void requestACategoryNotFound_shouldReturnAStatusNotFound() {
+
+        Header authenticationHeader = getAuthenticationHeader();
+
+        Response response =
+            given()
+                .header("Accept", APPLICATION_JSON)
+                .header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
+                .header(authenticationHeader)
+            .get(CATEGORY_URL + "/99999")
+                .thenReturn();
+
+        assertEquals(NOT_FOUND.value(), response.getStatusCode());
     }
 }
