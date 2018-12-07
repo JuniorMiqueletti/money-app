3852835e198dbe2cbbc4a83da060f74f4d9b1d7d
Junior Miqueletti <juniormiqueletti@gmail.com>
2018-12-06 23:38:34 -0200

    Adding Category creation test

--- src/test/java/com/juniormiqueletti/moneyapp/resource/CategoryRSIT.java
+++ src/test/java/com/juniormiqueletti/moneyapp/resource/CategoryRSIT.java
@@ -1,21 +1,23 @@
 package com.juniormiqueletti.moneyapp.resource;
 
+import com.juniormiqueletti.moneyapp.model.Category;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.junit.Test;
-import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.http.HttpStatus.*;
 
 public class CategoryRSIT extends AuthenticableTest {
 
     private static final String CONTENT_TYPE_HEADER = "Content-Type";
     private static final String APPLICATION_JSON = "application/json";
-    private static final int UNAUTHORIZED_HTTP_STATUS_CODE = 401;
-    private static final int OK_HTTP_STATUS_CODE = 200;
     private static final String CATEGORY_URL = "/api/v1/category";
+    private static final String LOCATION_HEADER = "location";
+    private static final String ID_REGEX = "[0-9]+";
+    private static final String LOCATION_PATTERN = ".*" + CATEGORY_URL +  "/" + ID_REGEX;
 
     @Test
     public void getCategoryWithoutToken() {
@@ -29,7 +31,7 @@ public class CategoryRSIT extends AuthenticableTest {
 
         String errorDescription = response.getBody().jsonPath().getString("error_description");
 
-        assertEquals(UNAUTHORIZED_HTTP_STATUS_CODE, response.getStatusCode());
+        assertEquals(UNAUTHORIZED.value(), response.getStatusCode());
         assertEquals(errorDescription, "Full authentication is required to access this resource");
     }
 
@@ -46,7 +48,7 @@ public class CategoryRSIT extends AuthenticableTest {
             .get(CATEGORY_URL + "/1")
                 .thenReturn();
 
-        assertEquals(OK_HTTP_STATUS_CODE, response.getStatusCode());
+        assertEquals(OK.value(), response.getStatusCode());
     }
 
     @Test
@@ -77,6 +79,29 @@ public class CategoryRSIT extends AuthenticableTest {
             .get(CATEGORY_URL)
                 .thenReturn();
 
-        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+        assertEquals(OK.value(), response.getStatusCode());
+    }
+
+    @Test
+    public void createACategory_shouldReturnOkWithLocationHeader() {
+        Header authenticationHeader = getAuthenticationHeader();
+
+        Category category2Create = new Category();
+        category2Create.setName("Financial");
+
+        Response response =
+            given()
+                .body(category2Create)
+                .header("Accept", APPLICATION_JSON)
+                .header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
+                .header(authenticationHeader)
+            .when()
+                .post(CATEGORY_URL)
+            .thenReturn();
+
+        assertEquals(CREATED.value(), response.getStatusCode());
+        String location = response.getHeader(LOCATION_HEADER);
+
+        assertTrue(location.matches(LOCATION_PATTERN));
     }
 }
